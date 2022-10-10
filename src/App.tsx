0d8b5b0f00739425ae40154c93b7f0b23ae64573
Your Name <you@example.com>
2022-10-10 15:15:08 +1300

    app restructure

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import React, { useState, useEffect } from 'react';
 
 import Container from '@mui/material/Container';
-import Grid from '@mui/material/Unstable_Grid2';
 import { ThemeProvider } from '@mui/material/styles';
 
-import { theme } from '../theme';
-import Screen from './Screen';
-import CalculatorButton from './CalculatorButton';
+import { theme } from './theme';
+import { MacCalculator } from '@/MacCalculator';
 
 type Value = String | Number;
 
 export const App = () => {
+  const [change, setChange] = useState<String>('mac');
   const [number, setNumber] = useState<any>([]);
   const [sign, setSign] = useState<String>("");
   const [reset, setReset] = useState<number>(0);
@@ -90,63 +89,13 @@ export const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <Container maxWidth="xs">
-        <Screen value={screen} />
-        <Grid columns={4} sx={border} container>
-          {calcArr.map((item, index) =>
-            <Grid xs={item.span} key={index} >
-              <CalculatorButton value={item.value} onClick={handlerFunc} colour={item.colour} >{item.title}</CalculatorButton>
-            </Grid>
-          )}
-        </Grid>
+        {change === 'mac' && <MacCalculator screen={screen} onClick={handlerFunc} />}
       </Container>
     </ThemeProvider >
   )
 };
 
 export default App;
-
-const calcArr = [
-  { title: 'C', colour: 'gray.200', value: 'C', span: 1 },
-  { title: '+/-', colour: 'gray.200', value: "+/-", span: 1 },
-  { title: '%', colour: 'gray.200', value: "%", span: 1 },
-
-  { title: '÷', colour: 'primary.main', value: '/', span: 1 },
-
-  { title: 1, colour: 'gray.100', value: 1, span: 1 },
-  { title: 2, colour: 'gray.100', value: 2, span: 1 },
-  { title: 3, colour: 'gray.100', value: 3, span: 1 },
-
-  { title: 'X', colour: 'primary.main', value: 'X', span: 1 },
-
-  { title: 4, colour: 'gray.100', value: 4, span: 1 },
-  { title: 5, colour: 'gray.100', value: 5, span: 1 },
-  { title: 6, colour: 'gray.100', value: 6, span: 1 },
-
-  { title: '-', colour: 'primary.main', value: '-', span: 1 },
-
-  { title: 7, colour: 'gray.100', value: 7, span: 1 },
-  { title: 8, colour: 'gray.100', value: 8, span: 1 },
-  { title: 9, colour: 'gray.100', value: 9, span: 1 },
-
-  { title: '+', colour: 'primary.main', value: '+', span: 1 },
-
-  { title: 0, colour: 'gray.100', value: 0, span: 2 },
-  { title: '.', colour: 'gray.100', value: '.', span: 1 },
-
-  { title: '=', colour: 'primary.main', value: "=", span: 1 },
-];
-
-const border = {
-  '--Grid-borderWidth': '1px',
-  borderTop: 'var(--Grid-borderWidth) solid',
-  borderLeft: 'var(--Grid-borderWidth) solid',
-  borderColor: '#000',
-  '& > div': {
-    borderRight: 'var(--Grid-borderWidth) solid',
-    borderBottom: 'var(--Grid-borderWidth) solid',
-    borderColor: '#000',
-  },
-};
 
 const toLocaleString = (number: String) =>
   String(number).replace(/(?<!\..*)(\d)(?=(?:\d{3})+(?:\.|$))/g, "$1 ");
---
+++ src/MacCalculator/index.tsx
@@ -0,0 +1,72 @@
+import * as React from 'react';
+
+import Grid from '@mui/material/Unstable_Grid2';
+
+import Screen from './Screen';
+import CalculatorButton from './CalculatorButton';
+
+
+interface MacCalculator {
+    screen: any;
+    onClick: Function;
+}
+
+export const MacCalculator = (props: MacCalculator) => {
+    const { onClick, screen } = props
+    return (
+        <>
+            <Screen value={screen} />
+            <Grid columns={4} sx={border} container>
+                {calcArr.map((item, index) =>
+                    <Grid xs={item.span} key={index} >
+                        <CalculatorButton value={item.value} onClick={onClick} colour={item.colour} >{item.title}</CalculatorButton>
+                    </Grid>
+                )}
+            </Grid>
+        </>
+    )
+}
+
+
+const calcArr = [
+    { title: 'C', colour: 'gray.200', value: 'C', span: 1 },
+    { title: '+/-', colour: 'gray.200', value: "+/-", span: 1 },
+    { title: '%', colour: 'gray.200', value: "%", span: 1 },
+
+    { title: '÷', colour: 'primary.main', value: '/', span: 1 },
+
+    { title: 1, colour: 'gray.100', value: 1, span: 1 },
+    { title: 2, colour: 'gray.100', value: 2, span: 1 },
+    { title: 3, colour: 'gray.100', value: 3, span: 1 },
+
+    { title: 'X', colour: 'primary.main', value: 'X', span: 1 },
+
+    { title: 4, colour: 'gray.100', value: 4, span: 1 },
+    { title: 5, colour: 'gray.100', value: 5, span: 1 },
+    { title: 6, colour: 'gray.100', value: 6, span: 1 },
+
+    { title: '-', colour: 'primary.main', value: '-', span: 1 },
+
+    { title: 7, colour: 'gray.100', value: 7, span: 1 },
+    { title: 8, colour: 'gray.100', value: 8, span: 1 },
+    { title: 9, colour: 'gray.100', value: 9, span: 1 },
+
+    { title: '+', colour: 'primary.main', value: '+', span: 1 },
+
+    { title: 0, colour: 'gray.100', value: 0, span: 2 },
+    { title: '.', colour: 'gray.100', value: '.', span: 1 },
+
+    { title: '=', colour: 'primary.main', value: "=", span: 1 },
+];
+
+const border = {
+    '--Grid-borderWidth': '1px',
+    borderTop: 'var(--Grid-borderWidth) solid',
+    borderLeft: 'var(--Grid-borderWidth) solid',
+    borderColor: '#000',
+    '& > div': {
+        borderRight: 'var(--Grid-borderWidth) solid',
+        borderBottom: 'var(--Grid-borderWidth) solid',
+        borderColor: '#000',
+    },
+};
