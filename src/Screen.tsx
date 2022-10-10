8f757ea76a371c76c177280d83ea6360b85c0435
Your Name <you@example.com>
2022-10-10 15:04:45 +1300

    removed undefined type

--- src/Screen.tsx
+++ src/Screen.tsx
@@ -2,10 +2,7 @@ import * as React from 'react';
 
 import Box from '@mui/material/Box';
 
-type Val = String;
-
 export const Screen = ({ value }: any) => {
-    console.log(value)
     return (
         <Box sx={{
             backgroundColor: 'gray.300',
